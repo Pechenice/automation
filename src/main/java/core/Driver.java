597fe92205d45a77fadbc01acab9ce8d547eff01
Daria <pechenice@gmail.com>
2017-10-24 17:06:52 +0300

    add library for drivers drivers were removed from resources

--- src/main/java/core/Driver.java
+++ src/main/java/core/Driver.java
@@ -1,5 +1,9 @@
 package core;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -45,15 +49,15 @@ public class Driver {
         if (Driver.driver == null) {
             switch (System.getProperty("test.browser"))  {
                 case "chrome":
-                    System.setProperty("webdriver.chrome.driver", "src/main/resources/chrome/chromedriver.exe");
+                    ChromeDriverManager.getInstance().setup();
                     driver = new ChromeDriver();
                     break;
                 case "firefox":
-                    System.setProperty("webdriver.gecko.driver", "src/main/resources/chrome/firefox/geckodriver.exe");
+                    FirefoxDriverManager.getInstance().setup();
                     driver = new FirefoxDriver();
                     break;
                 case "iexplore":
-                    System.setProperty("webdriver.ie.driver", "src/main/resources/chrome/ie/IEDriverServer.exe");
+                    InternetExplorerDriverManager.getInstance().setup();
                     driver = new InternetExplorerDriver();
                     break;
                 default:
