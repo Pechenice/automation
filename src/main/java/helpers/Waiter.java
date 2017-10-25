158929502d6f90d9c1e287a935b31e8eaa822411
Daria <pechenice@gmail.com>
2017-10-25 10:54:32 +0300

    again last step

---
+++ src/main/java/helpers/Waiter.java
@@ -0,0 +1,13 @@
+package helpers;
+
+import core.Driver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Waiter {
+    private static final int timeout = Integer.parseInt(System.getProperty("test.timeouts"));
+
+    public static void waitFor (ExpectedCondition condition) {
+        new WebDriverWait(Driver.get(), timeout).until(condition);
+    }
+}
