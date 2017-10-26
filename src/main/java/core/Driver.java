b41aeed8d54e69cbe410ba12b9051faaec4271b2
Daria <pechenice@gmail.com>
2017-10-26 11:44:44 +0300

    strange shit is going here

--- src/main/java/core/Driver.java
+++ src/main/java/core/Driver.java
@@ -18,42 +18,42 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    private static WebDriver driver = null;
-
-    public Driver() {
-        //block constructor
-    }
-
-    public static WebDriver get() {
-        return driver;
-    }
-
-    protected static void init() {
-        if (Driver.driver == null) {
-            switch (PropertiesContainer.get("test.browser"))  {
-                case "chrome":
-                    ChromeDriverManager.getInstance().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    FirefoxDriverManager.getInstance().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                case "iexplore":
-                    InternetExplorerDriverManager.getInstance().setup();
-                    driver = new InternetExplorerDriver();
-                    break;
-                default:
-                    throw new AssertionError("Unsupported browser " + System.getProperty("test.browser"));
-            }
-            Driver.driver.manage().timeouts().implicitlyWait(Long.parseLong(System.getProperty("test.timeouts")), TimeUnit.SECONDS);
-        }
-    }
-
-    public static void quit() {
-        if (driver != null) {
-            Driver.get().quit();
-            driver = null;
-        }
-    }
+//    private static WebDriver driver = null;
+//
+//    public Driver() {
+//        //block constructor
+//    }
+//
+//    public static WebDriver get() {
+//        return driver;
+//    }
+//
+//    protected static void init() {
+//        if (Driver.driver == null) {
+//            switch (PropertiesContainer.get("test.browser"))  {
+//                case "chrome":
+//                    ChromeDriverManager.getInstance().setup();
+//                    driver = new ChromeDriver();
+//                    break;
+//                case "firefox":
+//                    FirefoxDriverManager.getInstance().setup();
+//                    driver = new FirefoxDriver();
+//                    break;
+//                case "iexplore":
+//                    InternetExplorerDriverManager.getInstance().setup();
+//                    driver = new InternetExplorerDriver();
+//                    break;
+//                default:
+//                    throw new AssertionError("Unsupported browser " + System.getProperty("test.browser"));
+//            }
+//            driver.manage().timeouts().implicitlyWait(Long.parseLong(PropertiesContainer.get("test.timeouts")), TimeUnit.SECONDS);
+//        }
+//    }
+//
+//    public static void quit() {
+//        if (driver != null) {
+//            Driver.get().quit();
+//            driver = null;
+//        }
+//    }
 }
--- src/test/java/GoToTest.java
+++ src/test/java/GoToTest.java
@@ -1,19 +1,21 @@
 import core.Driver;
+import core.PropertiesContainer;
 import core.TestBase;
 import org.testng.annotations.Test;
 import pages.PikabuHomePage;
 
-public class GoToTest extends TestBase {
+public class GoToTest {
     @Test
     public static void goTo() {
-        Driver.get().get(System.getProperty("test.baseUrl"));
-        System.out.println("Base Url is loaded");
+//        Driver.get().get(System.getProperty("test.baseUrl"));
+//        System.out.println("Base Url is loaded");
+        PropertiesContainer.get("test.timeouts");
     }
 
-    @Test
-    public static void enterSite() {
-        Driver.get().get(System.getProperty("test.baseUrl"));
-        PikabuHomePage homePage = new PikabuHomePage();
-        homePage.login("something", "pass12345");
-    }
+//    @Test
+//    public static void enterSite() {
+//        Driver.get().get(System.getProperty("test.baseUrl"));
+//        PikabuHomePage homePage = new PikabuHomePage();
+//        homePage.login("something", "pass12345");
+//    }
 }
